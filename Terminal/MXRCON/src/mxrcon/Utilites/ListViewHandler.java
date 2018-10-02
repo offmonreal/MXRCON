7e86d5c27b682463d1dfdefee3d872270c40a17f
Maxim Bezverkhnyy <offmonreal@yahoo.com>
2018-10-02 18:35:39 +0300

    Add rcon library

---
+++ Terminal/MXRCON/src/mxrcon/Utilites/ListViewHandler.java
@@ -0,0 +1,14 @@
+package mxrcon.Utilites;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
+//Свой класс хандлера для клика мышаком по элементу ListView
+public class ListViewHandler implements EventHandler<MouseEvent>
+{
+    @Override
+    public void handle(MouseEvent event)
+    {
+        //this method will be overrided in next step
+    }
+}
